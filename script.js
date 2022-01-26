7e5c84defc00b3dbe4f9039fa373ae0c5b49ec8c
valeria <val.lilou.rod@gmail.com>
2022-01-25 17:58:08 -0700

    js vars

---
+++ script.js
@@ -0,0 +1,7 @@
+// constants for password criterias
+const passwordText = document.querySelector("#password");
+
+const passwordUpper = document.querySelector("#checkUpper");
+const passwordLower = document.querySelector("#checkLower");
+const passwordNumber = document.querySelector("#checkNumeric");
+const passwordSpecial = document.querySelector("#checkSpecial");
