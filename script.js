4804d62d3c4b5bb1043a5b08e3c2c6f89f9d6119
valeria <val.lilou.rod@gmail.com>
2022-01-25 19:36:29 -0700

    whats next

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const passwordUpper = document.querySelector("#checkUpper");
 const passwordLower = document.querySelector("#checkLower");
 const passwordNumber = document.querySelector("#checkNumeric");
 const passwordSpecial = document.querySelector("#checkSpecial");
+
+// * ... NEXT function of button "generate"
+// when I click on "generate" a window pops up
+// then I am asked if i'd like an upper case in my password
+// then I input "y" or "n"
+// when the users input is given the next prompt is displayed
