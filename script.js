872bcf31dcbf7b8685bb79396119089c7a7d289d
valeria <val.lilou.rod@gmail.com>
2022-01-29 13:40:27 -0700

    next prompt

--- script.js
+++ script.js
@@ -8,13 +8,29 @@ const passwordSpecial = document.querySelector("#checkSpecial");
 
 // * ... NEXT function of button "generate"
 // when I click on "generate" a window pops up
+// form functions
+var lowerForm = document.getElementById("lower-prompt");
+var upperForm =
+    document.getElementById("upper-prompt");
+    
 function openForm(){
-    document.getElementById("upper-prompt").style.display = "block";
+    upperForm.style.display = "block";
 }
+ function closeForm(){
+    upperForm.style.display = "none"
+ }
+
+function nextForm(){
+ lowerForm.style.display = "block";
+ closeForm();
+}
+
 // then I am asked if i'd like an upper case in my password
 // then I input "y" or "n"
 // when the users input is given the next prompt is displayed
+// user selects YES
 
-function closeForm(){
-    document.getElementById("prompts").style.display = "none";
-}
+//*** NEXT figure out a function for displaying the next prompt after answering the first ***
+//var promptTwo = document.getElementById("lower-prompt").style.display = "block";
+// user selects NO
+
