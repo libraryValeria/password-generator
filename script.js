d188c12a7492f6a1857cf19caa0144730122d62a
valeria <val.lilou.rod@gmail.com>
2022-02-08 18:05:49 -0700

    get randoms

--- script.js
+++ script.js
@@ -16,33 +16,36 @@ var symbolPrompt = document.getElementById("symbol-prompt");
 
 // array for all prompts
 var prompts = [upperPrompt, lowerPrompt, numberPrompt, symbolPrompt];
-
-// open criteria form
+// open criteria form (UPPERCASE)
 function openForm() {
   prompts[0].style.display = "block";
 }
-
-// next criteria presents to the user
+// next criteria presents to the user (LOWERCASE)
 function lowerForm() {
-   prompts[1].style.display = "block";
-   prompts[0].style.display = "none";
-  }
-
+  prompts[1].style.display = "block";
+  prompts[0].style.display = "none";
+}
+// next criteria presents to the user (NUMBERS)
 function numberForm() {
   prompts[2].style.display = "block";
   prompts[1].style.display = "none";
 }
-
+// next criteria presents to the user (SYMBOLS)
 function symbolForm() {
   prompts[3].style.display = "block";
   prompts[2].style.display = "none";
 }
 
-// then I am asked if i'd like an upper case in my password
-// then I input "y" or "n"
-// when the users input is given the next prompt is displayed
-// user selects YES
+// GET random cases for each criteria
+var upperCase = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+var lowerCase = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+var numberCase = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 
-//*** NEXT figure out a function for displaying the next prompt after answering the first ***
-//var promptTwo = document.getElementById("lower-prompt").style.display = "block";
-// user selects NO
+function symbolsCase() {
+  const symbols = "!@#$%^&*(){}[]+=<>?/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// function collect user inputs for each criteria
+var answerYes = document.getElementsByName("answer-yes");
+var answerNo = document.getElementsByName("answer-no");
